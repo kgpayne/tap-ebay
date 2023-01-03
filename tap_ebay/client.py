def3279ed2a153086334ac9e82cc8412e068f50b
Ken Payne <kpsupernut@gmail.com>
2023-01-03 10:16:19 +0000

    feat: support item status (#2)

--- tap_ebay/client.py
+++ tap_ebay/client.py
@@ -1,44 +1,143 @@
 """Custom client handling, including eBayStream base class."""
 
+
 from __future__ import annotations
 
+import base64
+import json
 import logging
-from os import PathLike
-from pathlib import Path
-from typing import Any, Dict, Iterable, List, Optional, Union
+from datetime import datetime, timedelta, timezone
 
-import singer_sdk._singerlib as singer
+import requests
 from ebaysdk.exception import ConnectionError
 from ebaysdk.finding import Connection as Finding
-from singer_sdk.streams import Stream
+from ebaysdk.shopping import Connection as Shopping
 
 logger = logging.getLogger(__name__)
 
 
-class eBayClient:
-    def __init__(self, app_id):
-        self.app_id = app_id
+class oAuth_token(object):
+    def __init__(
+        self,
+        error=None,
+        access_token=None,
+        refresh_token=None,
+        refresh_token_expiry=None,
+        token_expiry=None,
+    ):
+        """
+        token_expiry: datetime in UTC
+        refresh_token_expiry: datetime in UTC
+        """
+        self.access_token = access_token
+        self.token_expiry = token_expiry
+        self.refresh_token = refresh_token
+        self.refresh_token_expiry = refresh_token_expiry
+        self.error = error
 
-    def get_finding_api(self):
-        return Finding(appid=self.app_id, config_file=None)
+    def __str__(self):
+        token_str = "{"
+        if self.error != None:
+            token_str += f'"error": "{self.error}"'
+        elif self.access_token != None:
+            token_str += (
+                '"access_token": "'
+                + self.access_token
+                + '", "expires_in": "'
+                + self.token_expiry.strftime("%Y-%m-%dT%H:%M:%S:%f")
+                + '"'
+            )
+            if self.refresh_token != None:
+                token_str += (
+                    ', "refresh_token": "'
+                    + self.refresh_token
+                    + '", "refresh_token_expire_in": "'
+                    + self.refresh_token_expiry.strftime("%Y-%m-%dT%H:%M:%S:%f")
+                    + '"'
+                )
+        token_str += "}"
+        return token_str
+
+
+class eBayClient:
+    def __init__(self, app_id, client_secret, redirect_uri):
+        self.app_id = app_id
+        self.client_id = app_id
+        self.client_secret = client_secret
+        self.redirect_uri = redirect_uri
+
+    def get_finding_api(self, site_id="EBAY-US"):
+        return Finding(appid=self.app_id, config_file=None, siteid=site_id)
+
+    def get_shopping_api(self, site_id="EBAY-US"):
+        token = self.get_shopping_api_token()
+        return Shopping(
+            appid=self.app_id,
+            iaf_token=token.access_token,
+            config_file=None,
+            siteid=site_id,
+        )
 
     def execute(self, api, verb, data: dict = {}):
         try:
+            # headers = api.build_request_headers(verb)
             return api.execute(verb, data)
         except ConnectionError as e:
             logger.error(f"Connection failed: {e}\n{e.response.dict()}")
             raise e
 
+    def _generate_request_headers(self):
+        """Generate oauth request flow headers."""
+        b64_encoded_credential = base64.b64encode(
+            f"{self.client_id}:{self.client_secret}".encode("utf-8")
+        )
+        return {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": f"Basic {b64_encoded_credential.decode('utf-8')}",
+        }
 
-class eBayStream(Stream):
-    """Stream class for eBay streams."""
+    def _generate_application_request_body(self, scopes):
+        return {
+            "grant_type": "client_credentials",
+            "redirect_uri": self.redirect_uri,
+            "scope": scopes,
+        }
 
-    def __init__(
-        self,
-        tap,
-        client,
-        schema: str | PathLike | dict[str, Any] | singer.Schema | None = None,
-        name: str | None = None,
-    ):
-        super().__init__(tap=tap, schema=schema, name=name)
-        self.client = client
+    def get_application_token(self, api_endpoint, scopes):
+        """Get application token.
+
+        Stores result in token object, and returns token object
+        """
+        logger.info("Trying to get a new application access token ... ")
+
+        headers = self._generate_request_headers()
+        body = self._generate_application_request_body(" ".join(scopes))
+        resp = requests.post(api_endpoint, data=body, headers=headers)
+        content = json.loads(resp.content)
+        token = oAuth_token()
+
+        if resp.status_code == requests.codes.ok:
+            token.access_token = content["access_token"]
+            # set token expiration time 5 minutes before actual expire time
+            token.token_expiry = (
+                datetime.now(timezone.utc)
+                + timedelta(seconds=int(content["expires_in"]))
+            ) - timedelta(minutes=5)
+
+        else:
+            token.error = f"{str(resp.status_code)}: " + content["error_description"]
+            logging.error(
+                "Unable to retrieve token.  Status code: %s - %s",
+                resp.status_code,
+                requests.status_codes._codes[resp.status_code],
+            )
+            logging.error(
+                "Error: %s - %s", content["error"], content["error_description"]
+            )
+        return token
+
+    def get_shopping_api_token(self):
+        return self.get_application_token(
+            api_endpoint="https://api.ebay.com/identity/v1/oauth2/token",
+            scopes=["https://api.ebay.com/oauth/api_scope"],
+        )
--- tap_ebay/streams.py
+++ tap_ebay/streams.py
@@ -3,13 +3,37 @@
 from __future__ import annotations
 
 from copy import deepcopy
+from os import PathLike
 from pathlib import Path
+from typing import Any
 
+import singer_sdk._singerlib as singer
 from ebaysdk.response import ResponseDataObject
-
-from tap_ebay.client import eBayStream
+from singer_sdk.streams import Stream
 
 SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
+
+
+class eBayStream(Stream):
+    """Stream class for eBay streams."""
+
+    def __init__(
+        self,
+        tap,
+        client,
+        schema: str | PathLike | dict[str, Any] | singer.Schema | None = None,
+        name: str | None = None,
+    ):
+        super().__init__(tap=tap, schema=schema, name=name)
+        self.client = client
+
+    @classmethod
+    def response_to_dict(cls, obj):
+        if isinstance(obj, ResponseDataObject):
+            obj = obj.__dict__
+            for key, value in obj.items():
+                obj[key] = cls.response_to_dict(value)
+        return obj
 
 
 class FindingStream(eBayStream):
@@ -30,19 +54,8 @@ class FindingStream(eBayStream):
         self.searches = searches
         self._api = None
 
-    @property
-    def api(self):
-        if self._api is None:
-            self._api = self.client.get_finding_api()
-        return self._api
-
-    @classmethod
-    def response_to_dict(cls, obj):
-        if isinstance(obj, ResponseDataObject):
-            obj = obj.__dict__
-            for key, value in obj.items():
-                obj[key] = cls.response_to_dict(value)
-        return obj
+    def get_api(self, site_id="EBAY-US"):
+        return self.client.get_finding_api(site_id=site_id)
 
     @staticmethod
     def get_all_items(client, api, verb, data, max_pages=100):
@@ -67,14 +80,57 @@ class FindingStream(eBayStream):
 
     def get_records(self, context: dict | None):
         for search in self.searches:
-            items = self.get_all_items(
+            for site_id in search.get("site_global_ids", ["EBAY-US"]):
+                api = self.get_api(site_id=site_id)
+                items = self.get_all_items(
+                    client=self.client,
+                    api=api,
+                    verb=search["verb"],
+                    data=search["data"],
+                    max_pages=search.get("max_pages", 100),
+                )
+                for item in items:
+                    record = self.response_to_dict(item)
+                    record["search_id"] = search["name"]
+                    yield record
+
+
+class ItemStatusStream(eBayStream):
+
+    name = "item_status"
+    primary_keys = ["itemId"]
+    replication_key = None
+    schema_filepath = SCHEMAS_DIR / "item_status.json"
+
+    def __init__(
+        self,
+        *args,
+        items: list[dict],
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+        self.items = items
+
+    def get_api(self, site_id="EBAY-US"):
+        return self.client.get_shopping_api(site_id=site_id)
+
+    @staticmethod
+    def get_item_status(client, api, verb, data, max_pages=100):
+        params = deepcopy(data)
+        params["paginationInput"] = {"entriesPerPage": 100}
+        response = client.execute(api, verb, params)
+        if hasattr(response.reply, "Item"):
+            return response.reply.Item
+
+    def get_records(self, context: dict | None):
+        for item in self.items:
+            api = self.get_api(site_id=item["site_id"])
+            item_status = self.get_item_status(
                 client=self.client,
-                api=self.api,
-                verb=search["verb"],
-                data=search["data"],
-                max_pages=search.get("max_pages", 100),
+                api=api,
+                verb="GetItemStatus",
+                data={"ItemID": item["id"]},
             )
-            for item in items:
-                record = self.response_to_dict(item)
-                record["search_id"] = search["name"]
-                yield record
+            if item_status:
+                item_status_dict = self.response_to_dict(item_status)
+                yield item_status_dict
